a5130adab719368098fa5652adfa4715da76b3b1
pascaill <147127122+pascaill@users.noreply.github.com>
2023-10-20 17:59:23 +0700

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,23 @@
+class Menu {
+    private String name;
+    private double price;
+    private String category;
+
+    public Menu(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
